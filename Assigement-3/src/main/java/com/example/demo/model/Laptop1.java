811ea8e33a080dbe4df8c23ad56e748226fbfb35
HarshalBhore <harshalbhore1692.hb@gmail.com>
2020-08-18 16:59:45 +0530

    Initial Commit

---
+++ Assigement-3/src/main/java/com/example/demo/model/Laptop1.java
@@ -0,0 +1,35 @@
+//package com.example.demo.model;
+//
+//import javax.persistence.Embeddable;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import com.fasterxml.jackson.annotation.JsonInclude;
+//import com.fasterxml.jackson.annotation.JsonInclude.Include;
+//import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+//
+//@Embeddable
+//@JsonInclude(JsonInclude.Include.NON_EMPTY)
+//public class Laptop1 {
+//	
+//	private String name;
+//	
+//	private long weight;
+//	public Laptop1() {
+//		super();
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public long getWeight() {
+//		return weight;
+//	}
+//	public void setWeight(long weight) {
+//		this.weight = weight;
+//	}
+//	
+//	
+//
+//}
